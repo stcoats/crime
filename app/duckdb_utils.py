09029bad2a8e8433938943af3755e2a8d106b1b6
stcoats <32770789+stcoats@users.noreply.github.com>
2025-03-21 10:32:16 +0700

    Update duckdb_utils.py

--- app/duckdb_utils.py
+++ app/duckdb_utils.py
@@ -1,7 +1,7 @@
 import duckdb
 
 def get_connection():
-    return duckdb.connect('/mnt/data/forensic.duckdb', read_only=False)
+    return duckdb.connect('/mnt/data/forensic.duckdb', read_only=True)
 
 
 
